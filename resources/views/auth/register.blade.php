d691f42f940633ca7728d7d449ac123ab2ff43ad
Marian Hutanu <h.marian914@gmail.com>
2022-01-12 17:05:47 +0100

    Creazione factory, seeder e dinamicizazzione shopPage

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 		<div class="container">
 			<div class="wrap-breadcrumb">
 				<ul>
-					<li class="item-link"><a href="/" class="link">home</a></li>
+					<li class="item-link"><a href="/" class="link">Home</a></li>
 					<li class="item-link"><span>Register</span></li>
 				</ul>
 			</div>
