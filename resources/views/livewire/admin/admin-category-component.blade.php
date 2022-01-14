8915874bfc49f77dd683cdc0e136e4ccd22b7cef
Marian Hutanu <h.marian914@gmail.com>
2022-01-14 17:11:23 +0100

    Work in progress

--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -12,7 +12,14 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-
+                        <div class="row">
+                            <div class="col-md-6">
+                                All Categories
+                            </div>
+                            <div class="col-md-6">
+                                <a href="{{ route('admin.addcategory') }}" class="btn btn-success pull-right pull-right">Add new</a>
+                            </div>
+                        </div>
                     </div>
                     <div class="panel body">
                         <table class="table table-striped">
@@ -26,9 +33,12 @@
                             </thead>
                             <tbody>
                                 @foreach ($categories as $category)
-                                    <td>{{$category->id}}</td>
-                                    <td>{{$category->name}}</td>
-                                    <td>{{$category->slug}}</td>
+                                    <tr>
+                                        <td>{{$category->id}}</td>
+                                        <td>{{$category->name}}</td>
+                                        <td>{{$category->slug}}</td>
+                                        <td></td>
+                                    </tr>
                                 @endforeach
                             </tbody>
                         </table>
